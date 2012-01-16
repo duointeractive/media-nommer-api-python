2b43024d34f337e4b97ba006a73c4d6171d215d0
Greg Taylor <gtaylor@duointeractive.com>
2012-01-16 16:28:09 -0500

    Updating format, changing cb_medium and cb_high to ab 125kbps.

--- media_nommer_api/presets/video_cb.py
+++ media_nommer_api/presets/video_cb.py
@@ -43,41 +43,41 @@ def cb_medium():
     return {
         'nommer': 'media_nommer.ec2nommerd.nommers.ffmpeg.FFmpegNommer',
         'options': [
-                {
-                    'outfile_options': [
-                        ('vcodec', 'libx264'),
-                        ('preset', 'medium'),
-                        ('vprofile', 'baseline'),
-                        ('b', '921k'),
-                        ('bufsize', '921k'),
-                        ('vf', "yadif,scale='640:trunc(ow/a/2)*2'"),
-                        ('pass', '1'),
-                        ('f', 'mp4'),
-                        # Keyframe
-                        ('keyint_min', '60'),
-                        ('g', '120'),
-                        ('an', None),
-                    ],
-                },
-                {
-                    'outfile_options': [
-                        ('vcodec', 'libx264'),
-                        ('preset', 'medium'),
-                        ('vprofile', 'baseline'),
-                        ('b', '921k'),
-                        ('bufsize', '921k'),
-                        ('vf', "yadif,scale='640:trunc(ow/a/2)*2'"),
-                        ('pass', '2'),
-                        ('acodec', 'libfaac'),
-                        ('ab', '96k'),
-                        ('async', '480'),
-                        # Keyframe
-                        ('keyint_min', '60'),
-                        ('g', '120'),
-                        ('f', 'mp4'),
-                    ],
-                'move_atom_to_front': True,
-                },
+            {
+                'outfile_options': [
+                    ('vcodec', 'libx264'),
+                    ('preset', 'medium'),
+                    ('vprofile', 'baseline'),
+                    ('b', '921k'),
+                    ('bufsize', '921k'),
+                    ('vf', "yadif,scale='640:trunc(ow/a/2)*2'"),
+                    ('pass', '1'),
+                    ('f', 'mp4'),
+                    # Keyframe
+                    ('keyint_min', '60'),
+                    ('g', '120'),
+                    ('an', None),
+                ],
+            },
+            {
+                'outfile_options': [
+                    ('vcodec', 'libx264'),
+                    ('preset', 'medium'),
+                    ('vprofile', 'baseline'),
+                    ('b', '921k'),
+                    ('bufsize', '921k'),
+                    ('vf', "yadif,scale='640:trunc(ow/a/2)*2'"),
+                    ('pass', '2'),
+                    ('acodec', 'libfaac'),
+                    ('ab', '125k'),
+                    ('async', '480'),
+                    # Keyframe
+                    ('keyint_min', '60'),
+                    ('g', '120'),
+                    ('f', 'mp4'),
+                ],
+            'move_atom_to_front': True,
+            },
         ],
     }
 
@@ -111,7 +111,7 @@ def cb_high():
                     ('vf', "yadif,scale='1280:trunc(ow/a/2)*2'"),
                     ('pass', '2'),
                     ('acodec', 'libfaac'),
-                    ('ab', '96k'),
+                    ('ab', '125k'),
                     ('async', '480'),
                     ('keyint_min', '60'),
                     ('g', '120'),
